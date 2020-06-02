1cd3800e84fbf1d651ac6e7a652639026e081151
Wilton <wilton.hurtado@uptc.edu.co>
2020-06-02 15:39:15 -0500

    Se agrego imagnes

---
+++ img/mario.js
@@ -0,0 +1,14 @@
+﻿(function(window) {
+Símbolo_1 = function() {
+	this.initialize();
+}
+Símbolo_1._SpriteSheet = new createjs.SpriteSheet({images: ["mario.png"], frames: [[0,0,100,100,0,0,0],[100,0,100,100,0,0,0],[200,0,100,100,0,0,0],[300,0,100,100,0,0,0],[400,0,100,100,0,0,0],[500,0,100,100,0,0,0]]});
+var Símbolo_1_p = Símbolo_1.prototype = new createjs.Sprite();
+Símbolo_1_p.Sprite_initialize = Símbolo_1_p.initialize;
+Símbolo_1_p.initialize = function() {
+	this.Sprite_initialize(Símbolo_1._SpriteSheet);
+	this.paused = false;
+}
+window.Símbolo_1 = Símbolo_1;
+}(window));
+
